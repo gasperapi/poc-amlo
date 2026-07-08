ac02e1a379c06cbca686e5e94eeda4d3c442498f
Akaphon Mahapon <akaphon.mahaphon@gmail.com>
2026-07-08 13:47:38 +0700

    Update page redirection to prototype in site navigation

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,5 @@
-import {HomePage} from "../../views/site/home-page";
+import {redirect} from "next/navigation";
 
 export default function Page() {
-  return <HomePage />;
+  redirect("/prototype");
 }
